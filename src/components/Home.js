2bbec1f7b8a208b78a22aedf563bdf2703414d5a
Jo Keizer <jolanda@sociossports.nl>
2020-03-31 17:12:18 +0200

    deployment now added

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
             <div className="game-intro">
                 <div className="game-wrapper">
                     <div className="logo">
-                        <img src="../../../logo.png"/>
+                        <img src="/../../../images/logo.png"/>
                     </div>
                     <h1>Welkom bij de sport Quiz van Content Stadium</h1>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged
--- src/components/Quiz-data.js
+++ src/components/Quiz-data.js
@@ -1,18 +1,17 @@
 export const QuizData = [
     {
         id: 0,
-        question: "Which racer holds the record for the most Grand Prix wins?",
-        options: ["Sebastian Vettel", "Lewis hamilton ", "Max Verstappen", "Michael Schumacher"],
-        image: "../../../images/voetbal.jpg",
-        answer: "Michael Schumacher"
-
+        question: "From witch year is this picture? ",
+        options: ["1989", "1988 ", "1987", "1990"],
+        image: "/../../../images/voetbal.jpg",
+        answer: "1988"
     },
     {
         id: 1,
-        question: "When Michael Jordan played for the Chicago Bulls, how many NBA Championships did he win?",
-        options: ["8", "2", "6", "9"],
-        image: "",
-        answer: "6"
+        question: "The Hand of God goal from Diego Maradona was scored against?",
+        options: ["Brazil", "England", "Netherlands", "Spain"],
+        image: "/../../../images/maradona.jpg",
+        answer: "England"
     },
     {
         id: 2,
@@ -34,5 +33,19 @@ export const QuizData = [
         options: ["1910", "1887", "1877", "1904"],
         image: "",
         answer: "1877"
-    }
+    },
+    {
+        id: 5,
+        question: "Which racer holds the record for the most Grand Prix wins?",
+        options: ["Sebastian Vettel", "Lewis hamilton ", "Max Verstappen", "Michael Schumacher"],
+        image: "",
+        answer: "Michael Schumacher"
+    },
+    {
+        id: 6,
+        question: "Which of these events is NOT part of a decathlon?",
+        options: ["Javelin", "1500m", "Hammer throw", "Pole vault"],
+        image: "",
+        answer: "Hammer Throw"
+    },
 ]
